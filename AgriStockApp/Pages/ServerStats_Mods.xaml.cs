b511021da26a1cfa9e985d585397d3b98fa2a44e
Rony-Beck <microsoft@lafermenumerique.com>
2021-12-18 09:46:16 +0100

    Add Bulk Download 1/2

--- AgriStockApp/Pages/ServerStats_Mods.xaml.cs
+++ AgriStockApp/Pages/ServerStats_Mods.xaml.cs
@@ -2,6 +2,8 @@
 using Microsoft.VisualBasic;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Net;
@@ -14,7 +16,7 @@ using System.Windows.Media.Imaging;
 
 namespace AgriStockApp.Pages
 {
-    public partial class ServerStats_Mods : UserControl
+    public partial class ServerStats_Mods : UserControl, INotifyPropertyChanged
     {
         //Props
         public string ModPath { get; set; }
@@ -23,21 +25,88 @@ namespace AgriStockApp.Pages
         public string Game { get; set; }
         public ImageSource GameImg { get; set; }
 
+        //PropsFull
+        private bool isDownloadEnabled;
+        public bool IsDownLoadEnabled
+        {
+            get { return isDownloadEnabled; }
+            set { isDownloadEnabled = value; Update("IsDownLoadEnabled"); }
+        }
+
+        private int modsNew;
+        public int ModsNew
+        {
+            get { return modsNew; }
+            set { modsNew = value; Update("ModsNew"); }
+        }
+
+        private int modsSynced;
+        public int ModsSynced
+        {
+            get { return modsSynced; }
+            set { modsSynced = value; Update("ModsSynced"); }
+        }
+
+        private int modsTotal;
+        public int ModsTotal
+        {
+            get { return modsTotal; }
+            set { modsTotal = value; Update("ModsTotal"); }
+        }
+
+        private int modsToUpdate;
+        public int ModsToUpdate
+        {
+            get { return modsToUpdate; }
+            set { modsToUpdate = value; Update("ModsToUpdate"); }
+        }
+
+        private int progressTotal;
+        public int ProgressTotal
+        {
+            get { return progressTotal; }
+            set { progressTotal = value; Update("ProgressTotal"); }
+        }
+
+        private int progressCurrent;
+        public int ProgressCurrent
+        {
+            get { return progressCurrent; }
+            set { progressCurrent = value; Update("ProgressCurrent"); }
+        }
+
+
         //Ctor
         public ServerStats_Mods(string xmlData)
         {
             InitializeComponent();
+            this.DataContext = this;
             WebPath = Config.FS_Host + "/mods/";
             LoadList(xmlData);
         }
 
+        //Lists used
+        static List<CustomLists.Mod> Mods = new List<CustomLists.Mod>();
+
         //Functions
+        //Property Changed
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void Update(string Name)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(Name));
+        }
+
         //Load & Display Modslist
         internal async void LoadList(string xmlData)
         {
+            //Reset list
+            Mods.Clear();
+            //Security
             if (xmlData == "error") { return; }
+            //Read datas
             dynamic serverData = await Task.Run(() => JsonConvert.DeserializeObject(xmlData));
-            
+
             //*************************************
             //Add Automatic game detector api based
             //*************************************
@@ -48,7 +117,7 @@ namespace AgriStockApp.Pages
                 GameFolder = "FarmingSimulator2022";
                 GameImg = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "Assets\\Img\\fs22.png", UriKind.Absolute));
             }
-            
+
             //Get Local Mods List
             ModPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\My Games\\" + GameFolder + "\\mods";
             if (!Directory.Exists(ModPath))
@@ -60,9 +129,24 @@ namespace AgriStockApp.Pages
             //Foreach Serverside Mod
             foreach (var mod in serverData.mods)
             {
+                //Création du mod
+                var ThisMod = new CustomLists.Mod();
+                //Ajout des premières données
+                ThisMod.Name = mod.description;
+                ThisMod.Author = mod.author;
+                ThisMod.Version = mod.version;
+                ThisMod.Hash = mod.hash;
+                ThisMod.FileNameExt = mod.name + ".zip";
+                ThisMod.Path = WebPath + ThisMod.FileNameExt;
+                ThisMod.IsNew = true;
+                
+                //Ajout au nombre de mods total
+                ModsTotal++;
+                ModsNew++;
+
                 //Construction de la Grid mère
                 Grid MotherGrid = new Grid();
-                MotherGrid.Height = 75;
+                MotherGrid.Height = 60;
                 modsList_Zone.Children.Add(MotherGrid);
                 
                 //Ajout des colonnes
@@ -103,6 +187,7 @@ namespace AgriStockApp.Pages
                 Manage.Background = new SolidColorBrush(Color.FromRgb(0, 151, 68));
                 Manage.Margin = new Thickness(10);
                 Manage.Click += AddMod;
+                Manage.Visibility = Visibility.Collapsed;
                 Grid.SetColumn(Manage, 2);
 
                 //Zone Img
@@ -119,18 +204,34 @@ namespace AgriStockApp.Pages
                 {
                     if (Path.GetFileName(item) == (string)mod.name + ".zip")
                     {
-                        //HashChecker
-                        string modsHash = await Task.Run(() => Fonctions.GetModHash(@Path.Combine(ModPath, Path.GetFileName(item))));
-                        Debug.WriteLine(">> Hash: " + modsHash);
-
-                        if (modsHash != (string)mod.hash)
+                        ThisMod.IsNew = false;
+                        try
                         {
-                            Manage.Content = (string)Application.Current.FindResource("update");
-                            Manage.ToolTip = (string)Application.Current.FindResource("update") + " " + (string)mod.name;
-                            Manage.Background = Brushes.Violet;
+                            //HashChecker
+                            string modsHash = await Task.Run(() => Fonctions.GetModHash(@Path.Combine(ModPath, Path.GetFileName(item))));
+                            Debug.WriteLine(">> Hash: " + modsHash);
+
+                            //Si Hash ne corresponds pas
+                            if (modsHash != (string)mod.hash)
+                            {
+                                ThisMod.IsUpdate = true;
+                                ModsToUpdate++;
+                                ModsNew--;
+                                Manage.Content = (string)Application.Current.FindResource("update");
+                                Manage.ToolTip = (string)Application.Current.FindResource("update") + " " + (string)mod.name;
+                                Manage.Background = Brushes.Violet;
+                                break;
+                            }
+                        }
+                        catch
+                        {
                             break;
                         }
 
+                        //Si hash corresponds
+                        ThisMod.IsInstalled = true;
+                        ModsSynced++;
+                        ModsNew--;
                         Manage.Content = (string)Application.Current.FindResource("remove");
                         Manage.ToolTip = (string)Application.Current.FindResource("remove") + " " + (string)mod.name;
                         Manage.Background = Brushes.DarkOrange;
@@ -140,52 +241,79 @@ namespace AgriStockApp.Pages
                     }
                 }
 
+                //Add Mod to lists
+                Mods.Add(ThisMod);
+
                 //Display
                 MotherGrid.Children.Add(Details);
                 MotherGrid.Children.Add(Manage);
                 MotherGrid.Children.Add(ModImg);
             }
+
+            //Active Sync Button
+            ProgressCircle.Visibility = Visibility.Collapsed;
+            SyncButtonIcon.Visibility = Visibility.Visible;
+            if (ModsSynced != ModsTotal)
+            {
+                SyncButton.IsEnabled = true;
+                SyncButtonText.Text = (string)Application.Current.FindResource("downLoad");
+                SyncButtonIcon.Kind = MaterialDesignThemes.Wpf.PackIconKind.Download;
+            }
+            else
+            {
+                SyncButtonText.Text = (string)Application.Current.FindResource("upToDate");
+                SyncButtonIcon.Kind = MaterialDesignThemes.Wpf.PackIconKind.CheckboxMarkedCircleOutline;
+            }
         }
         
         //Telechargement
-        private static async Task DownloadFileAsync(string url, string filePath, Button source)
+        private async Task DownloadFileAsync(string url, string filePath, bool isNew, bool isUpdate)
         {
             WebClient client = new WebClient();
-            client.DownloadProgressChanged += (s, e) => { source.Content = e.ProgressPercentage + "%"; };
+            
+            //Progress Update
+            client.DownloadProgressChanged += (s, e) => { ProgressCurrent = e.ProgressPercentage; };
+            
+            //Démarrage du DL
             await client.DownloadFileTaskAsync(new Uri(url), filePath);
+
+            //Quand DL terminé
+            ModsSynced++;
+            if (isNew) ModsNew--;
+            if (isUpdate) ModsToUpdate--;
         }
 
         //Boutons
         //Ajouter un mod
-        private async void AddMod(object sender, RoutedEventArgs e)
+        private void AddMod(object sender, RoutedEventArgs e)
         {
-            Button source = e.Source as Button;
-            Grid pGrid = source.Parent as Grid;
-            StackPanel pDetails = pGrid.Children[0] as StackPanel;
-            TextBlock pModeName = pDetails.Children[0] as TextBlock;
+            //Button source = e.Source as Button;
+            //Grid pGrid = source.Parent as Grid;
+            //StackPanel pDetails = pGrid.Children[0] as StackPanel;
+            //TextBlock pModeName = pDetails.Children[0] as TextBlock;
 
-            string WebModsPath = WebPath + pModeName.Text;
+            //string WebModsPath = WebPath + pModeName.Text;
 
-            source.IsEnabled = false;
-            source.Content = (string)Application.Current.FindResource("wait");
+            //source.IsEnabled = false;
+            //source.Content = (string)Application.Current.FindResource("wait");
 
-            try
-            {
-                await DownloadFileAsync(WebModsPath, Path.Combine(ModPath, pModeName.Text), source);
+            //try
+            //{
+            //    await DownloadFileAsync(WebModsPath, Path.Combine(ModPath, pModeName.Text), source);
 
-                source.Click -= AddMod;
-                source.Click += RemoveMod;
+            //    source.Click -= AddMod;
+            //    source.Click += RemoveMod;
 
-                source.Content = (string)Application.Current.FindResource("remove");
-                source.ToolTip = (string)Application.Current.FindResource("remove") + " " + pModeName.Text;
-                source.Background = Brushes.DarkOrange;
-            }
-            catch (Exception ex)
-            {
-                source.Content = (string)Application.Current.FindResource("downLoad");
-                Debug.WriteLine(ex);
-            }
-            source.IsEnabled = true;
+            //    source.Content = (string)Application.Current.FindResource("remove");
+            //    source.ToolTip = (string)Application.Current.FindResource("remove") + " " + pModeName.Text;
+            //    source.Background = Brushes.DarkOrange;
+            //}
+            //catch (Exception ex)
+            //{
+            //    source.Content = (string)Application.Current.FindResource("downLoad");
+            //    Debug.WriteLine(ex);
+            //}
+            //source.IsEnabled = true;
         }
 
         //Retirer un mod
@@ -198,7 +326,8 @@ namespace AgriStockApp.Pages
 
             source.IsEnabled = false;
 
-            try {
+            try
+            {
                 File.Delete(Path.Combine(ModPath, pModeName.Text));
                 source.Click += AddMod;
                 source.Click -= RemoveMod;
@@ -213,6 +342,28 @@ namespace AgriStockApp.Pages
             }
 
             source.IsEnabled = true;
+        }
+
+        private async void SyncButton_Click(object sender, RoutedEventArgs e)
+        {
+            SyncButton.IsEnabled = false;
+            SyncButtonText.Text = (string)Application.Current.FindResource("updating");
+            ProgressCircle.Visibility = Visibility.Visible;
+            SyncButtonIcon.Visibility = Visibility.Collapsed;
+            foreach (var mod in Mods)
+            {
+                //Si déjà installé, zap au prochain
+                if (mod.IsInstalled || mod.Path == null) { continue; }
+
+                //Télécharge le mod
+                await DownloadFileAsync(mod.Path, Path.Combine(ModPath, mod.FileNameExt), mod.IsNew, mod.IsUpdate);
+                SyncButton.IsEnabled = false;
+            }
+
+            ProgressCircle.Visibility = Visibility.Collapsed;
+            SyncButtonIcon.Visibility = Visibility.Visible;
+            SyncButtonIcon.Kind = MaterialDesignThemes.Wpf.PackIconKind.CheckboxMarkedCircleOutline;
+            SyncButtonText.Text = (string)Application.Current.FindResource("done");
         }
     }
 }
---
+++ AgriStockApp/Scripts/CustomLists.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Media;
+
+namespace AgriStockApp.Scripts
+{
+    class CustomLists
+    {
+        //Represente les données d'un mod
+        public class Mod
+        {
+            public string Name { get; set; }
+            public string Hash { get; set; }
+            public string Author { get; set; }
+            public string Version { get; set; }
+            public string FileNameExt { get; set; }
+            public string Path { get; set; }
+            public ImageSource Image { get; set; }
+            public bool IsInstalled { get; set; }
+            public bool IsNew { get; set; }
+            public bool IsUpdate { get; set; }
+        }
+    }
+}
