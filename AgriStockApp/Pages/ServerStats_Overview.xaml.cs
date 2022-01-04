8ec8a0f38592f860588403ffb58637d9f080ce17
Rony-Beck <microsoft@lafermenumerique.com>
2022-01-04 05:36:50 +0100

    Starting Overview Page

--- AgriStockApp/Pages/ServerStats_Overview.xaml.cs
+++ AgriStockApp/Pages/ServerStats_Overview.xaml.cs
@@ -1,6 +1,8 @@
-﻿using Newtonsoft.Json;
+﻿using AgriStockApp.Scripts;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -17,27 +19,112 @@ using System.Windows.Shapes;
 
 namespace AgriStockApp.Pages
 {
-    public partial class ServerStats_Overview : UserControl
+    public partial class ServerStats_Overview : UserControl, INotifyPropertyChanged
     {
+        //Props Server
+        private string createdDateData;
+        public string CreatedDateData
+        {
+            get { return createdDateData; }
+            set { createdDateData = value; Update("CreatedDateData"); }
+        }
+
+        private string mapNameData;
+        public string MapNameData
+        {
+            get { return mapNameData; }
+            set { mapNameData = value; Update("MapNameData"); }
+        }
+
+        private string saveDateData;
+        public string SaveDateData
+        {
+            get { return saveDateData; }
+            set { saveDateData = value; Update("SaveDateData"); }
+        }
+
+        private int slotsUsageData;
+        public int SlotsUsageData
+        {
+            get { return slotsUsageData; }
+            set { slotsUsageData = value; Update("SlotsUsageData"); }
+        }
+
+        //Fields
+        Window window;
+        ContentControl statsPageHolder;
+
+        //Event
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void Update(string Name)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(Name));
+        }
+
         //Ctor
         public ServerStats_Overview(string xmlData)
         {
             InitializeComponent();
             Debug.WriteLine(">>> Opening ServerStats_Overview...");
-            Debug.WriteLine(xmlData);
+            this.DataContext = this;
 
-            if (xmlData == "off" || xmlData == "idle") { return; }
-
-            SetData(JsonConvert.DeserializeObject(xmlData));
+            //Trigger
+            this.Loaded += Ready;
         }
 
         //Functions
+        private void Ready(object sender, RoutedEventArgs e)
+        {
+            MainWindow._servUpdate += new serverUpdateEventHandler(AutoRefresh);
+            statsPageHolder = (ContentControl)this.Parent;
+            if (MainWindow.CareerSavegame == "off" || MainWindow.CareerSavegame == "idle" || MainWindow.CareerSavegame == "error") { return; }
+
+            SetData(JsonConvert.DeserializeObject(MainWindow.CareerSavegame));
+            SetMinimap();
+        }
+
+        private void AutoRefresh()
+        {
+            //If not active page => Skip
+            window = Window.GetWindow(this);
+            if (window == null) return;
+
+            //Else
+            SetData(JsonConvert.DeserializeObject(MainWindow.CareerSavegame));
+            SetMinimap();
+        }
+
         internal void SetData(dynamic json)
         {
+            string xData = MainWindow.ServerData;
+            //if offline
+            if (xData == "error")
+            {
+                return;
+            }
+            //Else
+            dynamic serverData = JsonConvert.DeserializeObject(xData);
+            MapNameData = serverData.server.mapName;
+
             //dynamic serverData = JsonConvert.DeserializeObject(json);
             dynamic data = json.careerSavegame.settings;
-            Created.Text = data.creationDate;
-            Slots.Text = json.careerSavegame.slotSystem.slotUsage;
+
+            CreatedDateData = data.creationDate;
+            SaveDateData = data.saveDate;
+            SlotsUsageData = (int)json.careerSavegame.slotSystem.slotUsage;
+        }
+
+        internal void SetMinimap()
+        {
+            MiniMap.Source = new ImageSourceConverter().ConvertFromString(FS_Api.Map(MainWindow.Current_FS_Host, MainWindow.Current_FS_Key)) as ImageSource;
+        }
+
+        //Actions
+        private void MiniMap_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            ServerStats.NavFrom = "ServerStats_Map";
+            statsPageHolder.DataContext = new ServerStats_Map(MainWindow.ServerData);
         }
     }
 }
