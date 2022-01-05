a4c62be2027060945fc3c05f2cbe588bb2f518ae
Rony-Beck <microsoft@lafermenumerique.com>
2022-01-05 07:20:24 +0100

    Adding First Launch routine

--- AgriStockApp/MainWindow.xaml.cs
+++ AgriStockApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using AgriStockApp.Scripts;
+﻿using AgriStockApp.Properties;
+using AgriStockApp.Scripts;
 using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
@@ -29,36 +30,40 @@ namespace AgriStockApp
         {
             InitializeComponent();
             
+            //Hello :)
+            Debug.WriteLine("Hello!");
+
             //Setting App Language
             this.SetLanguageDictionary();
 
             //Setting Up Timer
             SetTimer();
-            
-            Debug.WriteLine("Hello!");
-            
-            //Will be removed later
-            Current_FS_Host = Config.FS_Host;
-            Current_FS_Key = Config.FS_Key;
-
-            //Get Server Datas
-            RefreshXml();
-
-            Debug.WriteLine("Ready to serve.");
-
+                        
             //Setting Landing Page
             pageHolder.DataContext = this.DataContext;
-            pageHolder.DataContext = new Pages.ServerStats(); ;
+            if (Settings.Default.ServerHost == String.Empty || Settings.Default.ServerKey == String.Empty)
+            {
+                //Redirect to settings...
+                pageHolder.DataContext = new Pages.Settings(true);
+            }
+            else
+            {
+                //Get Server Datas
+                RefreshXml();
+
+                //Redirect to a nice page...
+                pageHolder.DataContext = new Pages.ServerStats();
+            }
         }
 
         //Language Selector
         private void SetLanguageDictionary()
         {
-            string lang = "fr";
+            string lang = Settings.Default.Lang;
             ResourceDictionary dict = new ResourceDictionary();
             switch (lang)
             {
-                case "fr":
+                case "Fr":
                     //Application.Current.Resources.MergedDictionaries.Clear();
                     Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary() { Source = new Uri("..\\Language\\Fr.xaml", UriKind.Relative) });
                     dict.Source = new Uri("..\\Language\\Fr.xaml", UriKind.Relative);
@@ -85,7 +90,10 @@ namespace AgriStockApp
         private void ServerDatasUpdate(object sender, EventArgs e)
         {
             Debug.WriteLine("> ServerTimerTicked!");
-            RefreshXml();
+            if (Settings.Default.ServerHost != String.Empty && Settings.Default.ServerKey != String.Empty)
+            {
+                RefreshXml();
+            }
         }
 
         //Refresh Server Datas
@@ -94,15 +102,15 @@ namespace AgriStockApp
             Debug.WriteLine(">> Refreshing Server Datas...");
 
             //Get server datas
-            ServerData = await Task.Run(() => Fonctions.getServerData(FS_Api.Stats(Current_FS_Host, Current_FS_Key)));
+            ServerData = await Task.Run(() => Fonctions.getServerData(FS_Api.Stats(Settings.Default.ServerHost, Settings.Default.ServerKey)));
             Debug.WriteLine(">>> Server API refreshed...");
 
             //Get server savegames
-            CareerSavegame = await Task.Run(() => Fonctions.getServerData_XML_to_JSON(FS_Api.Career(Current_FS_Host, Current_FS_Key)));
+            CareerSavegame = await Task.Run(() => Fonctions.getServerData_XML_to_JSON(FS_Api.Career(Settings.Default.ServerHost, Settings.Default.ServerKey)));
             Debug.WriteLine(">>> Career Savegame refreshed...");
-            EconomySavegame = await Task.Run(() => Fonctions.getServerData_XML_to_JSON(FS_Api.Economy(Current_FS_Host, Current_FS_Key)));
+            EconomySavegame = await Task.Run(() => Fonctions.getServerData_XML_to_JSON(FS_Api.Economy(Settings.Default.ServerHost, Settings.Default.ServerKey)));
             Debug.WriteLine(">>> Economy Savegame refreshed...");
-            VehiclesSavegame = await Task.Run(() => Fonctions.getServerData_XML_to_JSON(FS_Api.Vehicles(Current_FS_Host, Current_FS_Key)));
+            VehiclesSavegame = await Task.Run(() => Fonctions.getServerData_XML_to_JSON(FS_Api.Vehicles(Settings.Default.ServerHost, Settings.Default.ServerKey)));
             Debug.WriteLine(">>> Vehicles Savegame refreshed...");
 
             //Trigg Update
