6ac5bb93a8979a9bef178009c0082dce02b04d76
Rony-Beck <microsoft@lafermenumerique.com>
2022-01-05 07:43:47 +0100

    Remove demo server config

--- AgriStockApp/Scripts/Config.cs
+++ AgriStockApp/Scripts/Config.cs
@@ -2,8 +2,5 @@
 {
     public static class Config
     {
-        //Farming Simulator Server
-        public static string FS_Host = "http://62.210.30.0:8080";
-        public static string FS_Key = "ac5a4ef640b8abb4875bcb48c22e5e58";
     }
 }
